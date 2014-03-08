3e9cd5dc7ffe7f4eb21028e690897943c97d57c6
Paul Chote <paul@chote.net>
2014-03-09 12:44:56 +1300

    Port PathfinderDebugOverlay to MapCells.

--- OpenRA.Mods.RA/World/PathfinderDebugOverlay.cs
+++ OpenRA.Mods.RA/World/PathfinderDebugOverlay.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License Information
 /*
- * Copyright 2007-2013 The OpenRA Developers (see AUTHORS)
+ * Copyright 2007-2014 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
  * available to you under the terms of the GNU General Public License
  * as published by the Free Software Foundation. For more information,
@@ -20,16 +20,17 @@ namespace OpenRA.Mods.RA
 	class PathfinderDebugOverlayInfo : TraitInfo<PathfinderDebugOverlay> { }
 	class PathfinderDebugOverlay : IRenderOverlay, IWorldLoaded
 	{
-		Dictionary<Player, int[,]> layers;
-		int refreshTick;
-		World world;
 		public bool Visible;
+
+		Dictionary<Player, int[]> layers;
+		World world;
+		int refreshTick;
 
 		public void WorldLoaded(World w, WorldRenderer wr)
 		{
 			world = w;
 			refreshTick = 0;
-			layers = new Dictionary<Player, int[,]>(8);
+			layers = new Dictionary<Player, int[]>(8);
 
 			// Enabled via Cheats menu
 			Visible = false;
@@ -37,17 +38,23 @@ namespace OpenRA.Mods.RA
 
 		public void AddLayer(IEnumerable<Pair<CPos, int>> cellWeights, int maxWeight, Player pl)
 		{
-			if (maxWeight == 0) return;
+			if (maxWeight == 0)
+				return;
 
-			int[,] layer;
+			int[] layer;
 			if (!layers.TryGetValue(pl, out layer))
 			{
-				layer = new int[world.Map.Size.Width, world.Map.Size.Height];
+				var s = world.Map.Size;
+				layer = new int[s.Width * s.Height];
 				layers.Add(pl, layer);
 			}
 
 			foreach (var p in cellWeights)
-				layer[p.First.X, p.First.Y] = Math.Min(128, layer[p.First.X, p.First.Y] + (maxWeight - p.Second) * 64 / maxWeight);
+			{
+				var mc = new MapCell(world.Map, p.First);
+				if (mc.IsInMap)
+					layer[mc.Index] = Math.Min(128, layer[mc.Index] + (maxWeight - p.Second) * 64 / maxWeight);
+			}
 		}
 
 		public void Render(WorldRenderer wr)
@@ -59,29 +66,29 @@ namespace OpenRA.Mods.RA
 			var doDim = refreshTick - world.WorldTick <= 0;
 			if (doDim) refreshTick = world.WorldTick + 20;
 
-			var viewBounds = wr.Viewport.CellBounds;
+			var clip = wr.Viewport.CellBounds;
 			foreach (var pair in layers)
 			{
 				var c = (pair.Key != null) ? pair.Key.Color.RGB : Color.PaleTurquoise;
 				var layer = pair.Value;
 
 				// Only render quads in viewing range:
-				for (var j = viewBounds.Top; j <= viewBounds.Bottom; ++j)
+				for (var j = clip.Top; j <= clip.Bottom; ++j)
 				{
-					for (var i = viewBounds.Left; i <= viewBounds.Right; ++i)
+					for (var i = clip.Left; i <= clip.Right; ++i)
 					{
-						if (layer[i, j] <= 0)
+						var mc = new MapCell(world.Map, i, j);
+						if (layer[mc.Index] <= 0)
 							continue;
 
-						var w = Math.Max(0, Math.Min(layer[i, j], 128));
+						var w = Math.Max(0, Math.Min(layer[mc.Index], 128));
 						if (doDim)
-							layer[i, j] = layer[i, j] * 5 / 6;
+							layer[mc.Index] = layer[mc.Index] * 5 / 6;
 
 						// TODO: This doesn't make sense for isometric terrain
-						var pos = world.CenterOfCell(new CPos(i, j));
+						var pos = world.CenterOfCell(mc.Location);
 						var tl = wr.ScreenPxPosition(pos - new WVec(512, 512, 0));
 						var br = wr.ScreenPxPosition(pos + new WVec(511, 511, 0));
-
 						qr.FillRect(RectangleF.FromLTRB(tl.X, tl.Y, br.X, br.Y), Color.FromArgb(w, c));
 					}
 				}
