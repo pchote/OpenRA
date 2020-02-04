a774a0eb4bcda90b6b59e9c7adb1e35ff48fda76
Paul Chote <pchote@users.noreply.github.com>
2020-02-04 23:09:40 +0000

    WIP: Create macOS disk image on macOS.

--- packaging/osx/buildpackage.sh
+++ packaging/osx/buildpackage.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 # OpenRA packaging script for macOS
 
-command -v curl >/dev/null 2>&1 || { echo >&2 "macOS packaging requires curl."; exit 1; }
-command -v markdown >/dev/null 2>&1 || { echo >&2 "macOS packaging requires markdown."; exit 1; }
-
-if [[ "$OSTYPE" != "darwin"* ]]; then
-	command -v cmake >/dev/null 2>&1 || { echo >&2 "macOS packaging requires cmake."; exit 1; }
-	command -v genisoimage >/dev/null 2>&1 || { echo >&2 "macOS packaging requires genisoimage."; exit 1; }
-fi
-
 LAUNCHER_TAG="osx-launcher-20191007"
 
 if [ $# -ne "2" ]; then
 	echo "Usage: $(basename "$0") tag outputdir"
-    exit 1
+	exit 1
+fi
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+	echo >&2 "macOS packaging requires a macOS host"
+	exit 1
 fi
 
 # Set the working dir to the location of this script
@@ -25,7 +22,7 @@ SRCDIR="$(pwd)/../.."
 BUILTDIR="$(pwd)/build"
 
 modify_plist() {
-    sed "s|$1|$2|g" "$3" > "$3.tmp" && mv "$3.tmp" "$3"
+	sed "s|$1|$2|g" "$3" > "$3.tmp" && mv "$3.tmp" "$3"
 }
 
 # Copies the game files and sets metadata
@@ -55,16 +52,6 @@ curl -s -L -O https://github.com/OpenRA/OpenRALauncherOSX/releases/download/${LA
 unzip -qq -d "${BUILTDIR}" launcher.zip
 rm launcher.zip
 
-# Background image is created from source svg in artsrc repository
-# exported to tiff at 72 + 144 DPI, then combined using
-# tiffutil -cathidpicheck bg.tiff bg2x.tiff -out background.tiff
-cp background.tiff "${BUILTDIR}/.background.tiff"
-
-# Finder metadata created using create-dsstore.sh
-cp DS_Store "${BUILTDIR}/.DS_Store"
-
-ln -s /Applications/ "${BUILTDIR}/Applications"
-
 modify_plist "{DEV_VERSION}" "${TAG}" "${BUILTDIR}/OpenRA.app/Contents/Info.plist"
 modify_plist "{FAQ_URL}" "http://wiki.openra.net/FAQ" "${BUILTDIR}/OpenRA.app/Contents/Info.plist"
 echo "Building core files"
@@ -76,13 +63,6 @@ make core
 make version VERSION="${TAG}"
 make install-core gameinstalldir="/Contents/Resources/" DESTDIR="${BUILTDIR}/OpenRA.app"
 popd > /dev/null || exit 1
-
-curl -s -L -O https://raw.githubusercontent.com/wiki/OpenRA/OpenRA/Changelog.md
-markdown Changelog.md > "${BUILTDIR}/OpenRA.app/Contents/Resources/CHANGELOG.html"
-rm Changelog.md
-
-markdown "${SRCDIR}/README.md" > "${BUILTDIR}/OpenRA.app/Contents/Resources/README.html"
-markdown "${SRCDIR}/CONTRIBUTING.md" > "${BUILTDIR}/OpenRA.app/Contents/Resources/CONTRIBUTING.html"
 
 populate_template "OpenRA - Red Alert.app" "ra" "Red Alert"
 delete_mods "OpenRA - Red Alert.app" "cnc" "d2k"
@@ -96,30 +76,55 @@ delete_mods "OpenRA - Dune 2000.app" "ra" "cnc"
 rm -rf "${BUILTDIR}/OpenRA.app"
 
 echo "Packaging disk image"
+hdiutil create build.dmg -format UDRW -volname "OpenRA" -fs HFS+ -srcfolder build
+DMG_DEVICE=$(hdiutil attach -readwrite -noverify -noautoopen "build.dmg" | egrep '^/dev/' | sed 1q | awk '{print $1}')
+sleep 2
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	hdiutil create -volname OpenRA -srcfolder build -ov -format UDZO "${OUTPUTDIR}/OpenRA-${TAG}.dmg"
-else
-	echo "Building libdmg-hfsplus"
+# Background image is created from source svg in artsrc repository
+# exported to tiff at 72 + 144 DPI, then combined using
+# tiffutil -cathidpicheck bg.tiff bg2x.tiff -out background.tiff
+mkdir "/Volumes/OpenRA/.background/"
+cp background.tiff "/Volumes/OpenRA/.background/background.tiff"
+cp ra.icns "/Volumes/OpenRA/.VolumeIcon.icns"
 
-	# Cloning is very slow, so fetch zip instead
-	curl -s -L -O https://github.com/OpenRA/libdmg-hfsplus/archive/master.zip || exit 3
-	unzip -qq master.zip
-	rm master.zip
-	pushd libdmg-hfsplus-master > /dev/null || exit 1
-	cmake . > /dev/null
-	make > /dev/null
-	popd > /dev/null || exit 1
+echo '
+   tell application "Finder"
+     tell disk "'OpenRA'"
+           open
+           set current view of container window to icon view
+           set toolbar visible of container window to false
+           set statusbar visible of container window to false
+           set the bounds of container window to {400, 100, 1040, 580}
+           set theViewOptions to the icon view options of container window
+           set arrangement of theViewOptions to not arranged
+           set icon size of theViewOptions to 72
+           set background picture of theViewOptions to file ".background:background.tiff"
+           make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
+           set position of item "'OpenRA - Tiberian Dawn.app'" of container window to {160, 106}
+           set position of item "'OpenRA - Red Alert.app'" of container window to {320, 106}
+           set position of item "'OpenRA - Dune 2000.app'" of container window to {480, 106}
+           set position of item "Applications" of container window to {320, 298}
+           set position of item ".background" of container window to {160, 298}
+           set position of item ".fseventsd" of container window to {160, 298}
+           set position of item ".VolumeIcon.icns" of container window to {160, 298}
+           update without registering applications
+           delay 5
+           close
+     end tell
+   end tell
+' | osascript
 
-	if [[ ! -f libdmg-hfsplus-master/dmg/dmg ]] ; then
-		echo "libdmg-hfsplus compilation failed"
-		exit 3
-	fi
+# HACK: Copy the volume icon again - something in the previous step seems to delete it...?
+cp ra.icns "/Volumes/OpenRA/.VolumeIcon.icns"
+SetFile -c icnC "/Volumes/OpenRA/.VolumeIcon.icns"
+SetFile -a C "/Volumes/OpenRA"
 
-	genisoimage -V OpenRA -D -R -apple -no-pad -o build.dmg build
-	libdmg-hfsplus-master/dmg/dmg dmg ./build.dmg "${OUTPUTDIR}/OpenRA-${TAG}.dmg"
-	rm build.dmg
-fi
+chmod -Rf go-w /Volumes/OpenRA
+sync
+sync
+
+hdiutil detach ${DMG_DEVICE}
+hdiutil convert build.dmg -format UDZO -imagekey zlib-level=9 -ov -o "${OUTPUTDIR}/OpenRA-${TAG}.dmg"
 
 # Clean up
-rm -rf libdmg-hfsplus-master "${BUILTDIR}"
+rm -rf "${BUILTDIR}" build.dmg
